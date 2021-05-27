8933d8be625b75797a59e89f049e731351514f24
s20 <che7111987@gmail.com>
2021-05-27 19:02:52 +0300

    done file

--- conftest.py
+++ conftest.py
@@ -5,23 +5,17 @@ from selenium.webdriver.chrome.options import Options
 # pytest -s -v --browser_name=chrome test_parser.py
 # pytest -s -v --browser_name=firefox test_parser.py
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
 
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='es',
+                     help="Choose lang")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
 import pytest
-from selenium.webdriver.chrome.options import Options
-
+import time
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -17,11 +16,13 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     go_to_login_page(browser)
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     browser.get(link)
-#     login_link = browser.find_element_by_css_selector("#login_link")
-#     login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+    time.sleep(5)
+
 
 
 if __name__ == "__main__":
