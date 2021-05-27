a2a028160336df44f470bf6c3716c096d0ae122d
s20 <che7111987@gmail.com>
2021-05-27 19:14:55 +0300

    add base_page,main_page

--- test_main_page.py
+++ test_main_page.py
@@ -12,9 +12,11 @@ def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login(browser):
     browser.get(link)
     go_to_login_page(browser)
+    time.sleep(5)
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
