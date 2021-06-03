8501e758f5a9809506ab4194cf6fb304fc3b3a98
s20 <che7111987@gmail.com>
2021-06-03 19:58:26 +0300

    fix test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 from .pages.product_page import ProductPage
+import sys
+import pytest
 import time
 from .pages.locators import ProductPageLocators
 
@@ -11,8 +13,9 @@ def test_add(browser):
     page.open()
     page.push_add_to_basket()
 
+
 def test_name_book(browser):
-    '''Return books name.'''
+    '''Return book`s name.'''
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
     page.open()
@@ -34,7 +37,7 @@ def test_add_names_book(browser):
 
 
 def test_price_book(browser):
-    '''Return books price'''
+    '''Return book`s price'''
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
     page.open()
@@ -50,9 +53,9 @@ def test_add_price_book(browser):
     page.add_price_book()
 
 
-def test_guest_can_add_product_to_basket(browser):
+def test_old_guest_can_add_product_to_basket(browser):
     '''Проверка цены и книги в корзине'''
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
     page = ProductPage(browser, link)
     page.open()
     name1 = browser.find_element(*ProductPageLocators.NAME_BOOK)
@@ -74,3 +77,37 @@ def test_guest_can_add_product_to_basket(browser):
     print(f"\tДобавлена книга: {book2}")
     assert (price1 == price2), "Цены разные"
     assert (book1 == book2), "Книги разные"
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    name1 = browser.find_element(*ProductPageLocators.NAME_BOOK)
+    book1 = name1.text
+    css_price1 = browser.find_element(*ProductPageLocators.PRICE_BOOK)
+    price1 = css_price1.text
+    page2 = ProductPage(browser, link)
+    page2.open()
+    add_basket = browser.find_element(*ProductPageLocators.BUTTON_BASKET)
+    add_basket.click()
+    promt = ProductPage(browser, link)
+    promt.solve_quiz_and_get_code()
+    time.sleep(2)
+    promt = browser.find_element(*ProductPageLocators.ADD_NAME_BOOK)
+    book2 = promt.text
+    css_price2 = browser.find_element(*ProductPageLocators.ADD_PRICE_BOOK)
+    price2 = css_price2.text
+    assert (price1 == price2), "Цены разные"
+    assert (book1 == book2), "Книги разные"
+
+
