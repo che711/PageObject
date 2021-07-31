bcf0ac360f4399fe7dc2f7b3f5d29d1e7bd2087f
che711 <che711@mail.ru>
2021-07-31 18:03:02 +0300

    add requirements.txt

--- test_main_page.py
+++ test_main_page.py
@@ -33,7 +33,7 @@ def test_register_form(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     page = LoginPage(browser, link)
     page.open()
-    page.should_be_register_form
+    page.should_be_register_form()
 
 def test_should_be_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
