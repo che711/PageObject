6020abb140620a3cea69a6d1d80b70156a352f8a
che711 <che711@mail.ru>
2021-07-31 18:50:46 +0300

    add --headless

--- test_main_page.py
+++ test_main_page.py
@@ -21,7 +21,7 @@ def test_login_form(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     page = LoginPage (browser, link)
     page.open()
-    page.should_be_login_form
+    page.should_be_login_form()
 
 def test_should_be_login_url(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
