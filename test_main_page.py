279e52223f9f5e7d6904c0a330aa0e553f5efbc8
che711 <che711@mail.ru>
2021-07-31 17:19:52 +0300

    add --headless

--- test_main_page.py
+++ test_main_page.py
@@ -39,5 +39,5 @@ def test_should_be_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_page
+    page.should_be_login_page()
 
